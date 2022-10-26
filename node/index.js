53837726c4f6a8a5bbef541ce0be8223ab5fdcef
Gabrielox20 <113552881+Gabrielox20@users.noreply.github.com>
2022-10-26 18:24:33 -0300

    Add files via upload

--- node/index.js
+++ node/index.js
@@ -18,79 +18,143 @@ const kafka = new Kafka({
 //somos producer
 const producer = kafka.producer();
 const consumer1 = kafka.consumer({ groupId: 'grupo sexo1' })
-const consumer2 = kafka.consumer({ groupId: 'grupo sexo2' })
 
-/*fs.readFile('users.json', (err, data) => {
-  if (err) throw err;
-  let student = JSON.parse(data);
-});*/
 
-let jsonData = require('./users.json');
 
-for(i in jsonData.usuarios){
-  console.log(jsonData.usuarios[i])
-  if(jsonData.usuarios[i].Premium=="Si"){
-    console.log(jsonData.usuarios[i])
+app.get("/listapremium", async (req, res) => {
+  // Producing
+  let jsonData = require('./users.json');
+  for(i in jsonData.premium){
+      jsonData.premium[i].numerolista = i;
+      await producer.connect()
+      await producer.send({
+        topic: 'test-topic',
+        messages: [
+          { key: 'Premium', value: JSON.stringify( jsonData.premium[i]), partition: 0, },
+        ]
+
+      })
   }
-  else{
-    console.log(jsonData.usuarios[i])
-  }
-}
-
-app.post("/", async (req, res) => {
-  // Producin
-  await producer.connect()
-  await producer.send({
-    topic: 'test-topic',
-    messages: [
-      { key: 'key1', value: jsonData.usuarios[0].Nombre, partition: 0 },
-      { key: 'key2', value: req.query.userPremium, partition: 1 }
-    ]
-  })
   await producer.disconnect()
-
   res.send({
-    mensaje: "enviado"
+    mensaje: "Lista Premium"
   })
-
+})
+app.get("/lista", async (req, res) => {
+  // Producing
+  let jsonData = require('./users.json');
+  for(i in jsonData.nopremium){
+    jsonData.nopremium[i].numerolista = i;
+    await producer.connect()
+      await producer.send({
+        topic: 'test-topic',
+        messages: [
+          { key: 'NoPremium', value: JSON.stringify(jsonData.nopremium[i]), partition: 1 , },
+        ],
+      }
+      )
+  }
+  await producer.disconnect()
+  res.send({
+    mensaje: "Lista Publicada"
+  })
 })
 
-app.get("/rx", async (req,res) => {
+app.post("/registro", async (req, res) => {
+  // Producing
+  const fileName = './users.json';
+  const file = require(fileName);
+  let nuevousuario = req.body
+  file.premium.push(nuevousuario)
+  console.log(file.premium)
+  /*let jsonData = require('./users.json');
+  */
+  fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('Guardando en ' + fileName);
+  });
 
-  await consumer1.connect()
-  await consumer1.subscribe({ topic: 'test-topic', fromBeginning: true })
+  await producer.disconnect()
+  res.send({
+    mensaje: "Usuario Publicado"
+  })
+})
+app.post("/aceptar", async (req, res) => {
+  // Producing
+  const fileName = './users.json';
+  const file = require(fileName);
+  let aceptado = req.query.Aceptado
+  let numero = parseInt(req.query.NumeroLista) 
+  let premium = req.query.UsuarioPremium
+  //console.log(premium)
+  if(premium === "Si") {
+    if(aceptado === "Si") {
+      console.log("Si")
+      res.send({
+        mensaje: "Usuario aceptado"
+      })
+    }
+    if(aceptado === "No") {
+      console.log("No")
+      console.log(file.nopremium[numero]) 
+      var deletefile = file.nopremium.splice(numero)
+      console.log(file.nopremium)
+      res.send({
+        mensaje: "Usuario No aceptado"
+      })
+    }
+    /*delete file.premium[numero]
+    console.log(file.premium)*/
+  }
+  if(premium === "No") {
+    if(aceptado === "Si") {
+      console.log("Si")
+      res.send({
+        mensaje: "Usuario aceptado"
+      })
+    }
+    if(aceptado === "No") {
+      console.log("No")
+      console.log(file.nopremium[numero]) 
+      var deletefile = file.nopremium.splice(numero)
+      console.log(file.nopremium)
+      res.send({
+        mensaje: "Usuario No aceptado"
+      })
+    }
+    
+  }
+
+  fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('Guardando en ' + fileName);
+  });
+
+  await producer.disconnect()
   
+})
+
+async function consumidor() {
+  consumer1.connect()
+  await consumer1.subscribe({ topic: 'test-topic', fromBeginning: true })
   await consumer1.run({
-    partitionsConsumedConcurrently: 2,
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
         console.log({
-            consumer:1,
-            partition,
             key: message.key.toString(),
-            value: message.value.toString(),
-            headers: message.headers,
+            value:JSON.parse(message.value.toString()),
+            partition,
         })
     },
 })
 
-  await consumer2.connect()
-  await consumer2.subscribe({ topic: 'test-topic', fromBeginning: true })
-  await consumer2.run({
-    partitionsConsumedConcurrently: 2,
-    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
-        console.log({
-            consumer:2,
-            partition,
-            key: message.key.toString(),
-            value: message.value.toString(),
-        })
-    },
-})
   res.send({
     mensaje: "recibido"
   })
-  });
+};
 
 app.listen(port, () => {
   console.log(`API RUN AT http://localhost:${port}`);
+  consumidor();
 });
